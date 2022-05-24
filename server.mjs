23d968d69b9d96be92da137622a24f890ea2bc1f
Jesus Murillo <jesuschuymurillo@gmail.com>
2022-05-23 23:26:07 -0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,7 +1,5 @@
 import express from 'express';
-import fetch from 'node-fetch';
 import 'dotenv/config';
-import asyncHandler from 'express-async-handler';
 import bodyParser from 'body-parser'
 
 const PORT = process.env.PORT
@@ -13,34 +11,47 @@ var userDb = [{user:{name: 'james', username: 'jamesisaboss', email: 'jamesisabo
 
 app.use(express.static('public'));
 
-// Handle GET requests to '/random-person' with a middle-ware function and a function to send a request
-// to "https://randomuser.me/api/", receive the response at the express server, and then return the response 
-// back to to random.js to then be displayed in the browser.
-
-// handle GET request and send a request to "https://randomuser.me/api/" and return that data in JSON form 
-//  back to be dealt with by random.js
+// Handle GET request for user login
 app.get("/sign-in", async (req,res) => {
     try{
-        res.send(userDb);
-        console.log("User retrieved")
+        res.send(userDb)
+        console.log(200)
     } catch{
         console.log(500)
     }
 });
 
+// Handle POST request for user creation
 app.post("/new-user", (req, res) => {
     try{
+        let message = ""
+        
+        // check whether or not user already exists
+        for(let i = 0; i < userDb.length; i++)
+        {
+            if(req.body.u == userDb[i]['user']['username'])
+            {
+                console.log("User already exists")
+                message = "User already exists"
+            }
+
+            else if(req.body.e == userDb[i]['user']['email'])
+            {
+                console.log("Account already exists for this email")
+                message = "Account already exists for this email"
+            }
+        }
+
         userDb.push({user:{name: req.body.n, username: req.body.u, email: req.body.e, password: req.body.p}})
-        console.log(userDb)
-        res.json(req.body)
+        if (message === "")
+            message = "Welcome " + name + "!"
+        res.send(message)
     }
     catch{
         console.log(500)
     }
 });
 
-
-// Note: Don't add or change anything below this line.
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
